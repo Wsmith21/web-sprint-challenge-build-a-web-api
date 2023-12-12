f2a51ea6da59b3918684cdefc4494f08e28a8086
walyn smithe <walynsmithe@yahooe.com>
2023-12-12 15:36:27 -0600

    updated

--- api/server.js
+++ api/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const server = express();
 
-
 server.use(express.json()); // Use JSON parser middleware
 // Other middleware configurations...
 
 // Configure your routes
-const projectsRouter = require('./projects-router');
-const actionsRouter = require('./actions-router');
+const projectsRouter = require('/Users/walynsmith/web-sprint-challenge-build-a-web-api/api/projects/projects-router.js');
+const actionsRouter = require('/Users/walynsmith/web-sprint-challenge-build-a-web-api/api/actions/actions-router.js');
 
-server.use('./projects-router', projectsRouter);
-server.use('./actions-router', actionsRouter);
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 9002;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
